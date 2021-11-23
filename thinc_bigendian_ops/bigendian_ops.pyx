074a3b42f5e40648154dda97288d793a8b8e32eb
Andrew M. Sica <andrewsi@us.ibm.com>
2021-11-23 12:42:48 -0500

    remove hash

--- thinc_bigendian_ops/bigendian_ops.pyx
+++ thinc_bigendian_ops/bigendian_ops.pyx
@@ -36,31 +36,3 @@ class BigEndianOps(NumpyOps):
             return out.byteswap().newbyteorder()
         else:
             return out
-
-        
-    @cython.boundscheck(False)
-    @cython.wraparound(False)
-    def hash(self, const uint64_t[::1] ids, uint32_t seed):
-        """Hash a sequence of 64-bit keys into a table with 4 32-bit keys."""
-        # Written to mirror the GPU implementation
-        cdef np.ndarray[uint32_t, ndim=2] keys = self.alloc((ids.shape[0], 4), dtype='uint32')
-        cdef int i, j
-        cdef unsigned char entropy[16] # 128/8=16
-        cdef size_t n_items = len(ids)
-        cdef size_t in_size = sizeof(uint64_t)
-        cdef unsigned char src[16] 
-        dest = <unsigned char*>keys.data
-        # byteorder (endian) compatibility change:
-        #   numpy_ops hash maps src as a char array over input uint64_t id values. Each byte (char)
-        #   in each uint64 value is operated on by the underlying hash logic. However, this method of 
-        #   mapping the hash results a reversal of the resulting keys on big endian platforms. The
-        #   approach used here implements a byteorder agnostic approach that will result in an 
-        #   consistent result regardless of implementation platform. 
-        for i in range(n_items):
-            for k in range(in_size):
-                src[k] = (ids[i] >> (k*8)) & 0xFF
-            hash128_x64(<void*>src, in_size, seed, entropy)
-            for j in range(16):
-                dest[j] = entropy[j]
-            dest += 16
-        return keys 
